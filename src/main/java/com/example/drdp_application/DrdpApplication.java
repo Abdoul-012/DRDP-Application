37abcbcb22ec46e51fa9dd4a4351daf6cda28904
Abdoul <abdoul.diallo.spirite012@gmail.com>
2025-01-21 16:42:05 +0100

    Premier commit

---
+++ src/main/java/com/example/drdp_application/DrdpApplication.java
@@ -0,0 +1,13 @@
+package com.example.drdp_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrdpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/drdp_application/DrdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drdp_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
